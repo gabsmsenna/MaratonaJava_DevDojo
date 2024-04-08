0d39d5cc40760ff5ae34a91d009b764f8ace00b9
Gabriel Senna <gabrielmagalhaes3344@gmail.com>
2024-04-08 10:16:41 -0300

    Fixando conhecimento sobre String Builder, composição, date time e etc

---
+++ academy/nelioalves/cursojava/domain/Enumeração/Exercício02/Application/Program.java
@@ -0,0 +1,4 @@
+package academy.nelioalves.cursojava.domain.Enumeração.Exercício02.Application;
+
+public class Program {
+}
---
+++ academy/nelioalves/cursojava/domain/Enumeração/Exercício02/Entities/Comment.java
@@ -0,0 +1,4 @@
+package academy.nelioalves.cursojava.domain.Enumeração.Exercício02.Entities;
+
+public class Comment {
+}
