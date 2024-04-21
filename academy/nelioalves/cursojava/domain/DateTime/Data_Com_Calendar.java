e3d29742bd5ff94c82e14a3f9a5efc5acd490b4a
Gabriel <gabrielmagalhaes3344@gmail.com>
2024-04-21 18:37:29 -0300

    Revisando estudos de manipulação de datas

---
+++ academy/nelioalves/cursojava/domain/DateTime/Data_Com_Calendar.java
@@ -0,0 +1,26 @@
+package Repositorio_Estudos_JAVA.academy.nelioalves.cursojava.domain.DateTime;
+
+import java.sql.SQLOutput;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Data_Com_Calendar {
+    public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+
+        if (c.getFirstDayOfWeek() == Calendar.SUNDAY) {
+            System.out.println("Domingo é o primeiro dia da semana");
+        }
+
+        System.out.println(c.get(Calendar.DAY_OF_WEEK));
+        System.out.println(c.get(Calendar.DAY_OF_WEEK_IN_MONTH));
+        System.out.println(c.get(Calendar.DAY_OF_MONTH));
+        System.out.println(c.get(Calendar.DAY_OF_YEAR));
+
+        c.add(Calendar.DAY_OF_MONTH, 2);
+        c.add(Calendar.HOUR, 12);
+
+        Date date = c.getTime();
+        System.out.println(date);
+    }
+}
