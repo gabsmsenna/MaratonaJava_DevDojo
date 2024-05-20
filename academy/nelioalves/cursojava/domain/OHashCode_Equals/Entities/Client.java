eade50e88ca778bf1b6b2ae0061f0ca8fa0b733b
Gabriel <gabrielmagalhaes3344@gmail.com>
2024-05-20 16:51:30 -0300

    Hashcode e Equals

---
+++ academy/nelioalves/cursojava/domain/OHashCode_Equals/Entities/Client.java
@@ -0,0 +1,26 @@
+package Repositorio_Estudos_JAVA.academy.nelioalves.cursojava.domain.OHashCode_Equals.Entities;
+
+import java.util.Objects;
+
+public class Client {
+    private String name;
+    private String email;
+
+    public Client(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Client)) return false;
+        Client client = (Client) o;
+        return Objects.equals(name, client.name) && Objects.equals(email, client.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, email);
+    }
+}
